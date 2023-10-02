3c01940c056db6b36cea12bab56c8afab85d28e3
Ian Silva Santos <ian.santos@sptech.school>
2023-10-02 15:25:53 -0300

    Adicionando a tabela endereço, colocando alguns relacionamentos e fazendo a modelagem lógica v01

--- database/hardwareLife.sql
+++ database/hardwareLife.sql
@@ -1,45 +1,53 @@
 CREATE DATABASE HardwareLife;
 
+
+
 USE HardwareLife;
 
  -- TABELA PARA ADMINISTRAÇÃO -- 
  
 CREATE TABLE administrador(
-	id_admin int primary key auto_increment,
-    email_admin varchar(40),
-    nome_admin varchar(40),
+	idAdmin int primary key auto_increment,
+    emailAdmin varchar(40),
+    nomeAdmin varchar(40),
     senha varchar(40)
 );
 
  -- TABELA PARA CONTROLE E CADASTRO DE EMPRESAS CLIENTES --
  
  CREATE TABLE empresa(
-	id_empresa int primary key auto_increment,
+	idEmpresa int primary key auto_increment,
 	cnpj char(18),
-    logradouro varchar(60),
+    nomeEmpresa varchar(50),
+    razaoSocial varchar(100),
+    emailEmpresa varchar(50),
+    telefoneEmpresa varchar(12),
+    qtdSala varchar(15),
+    qtdRacks varchar(70)
+ );
+ 
+ CREATE TABLE endereco(
+	idEndereco int primary key auto_increment,
+	logradouro varchar(60),
     numero varchar(10),
     bairro varchar(60),
     cidade varchar(60),
     uf char(2),
     cep char(9),
-    nome_empresa varchar(50),
-    razao_social varchar(100),
-    email_empresa varchar(50),
-    telefone_empresa varchar(12),
-    qtdSala varchar(15),
-    qtdRacks varchar(70)
+    fkEmpresa int,
+    CONSTRAINT fkEmpresaEndereco FOREIGN KEY (fkEmpresa) REFERENCES Empresa(idEmpresa)
  );
 
  -- TABELA PARA CONTROLE E LOGIN DE CLEINTES --
  
 CREATE TABLE funcionario_empresa(
-	id_funcionario_empresa int primary key auto_increment,
-    email_funcionario_empresa varchar(50),
-    nome_funcionario_empresa varchar(40),
+	idFuncionario int primary key auto_increment,
+    emailFuncionario varchar(50),
+    nomeFuncionario varchar(40),
     cpf char(14),
     senha varchar(40),
 	fk_empresa int,
-     constraint foreign key (fk_empresa) references empresa(id_empresa)
+     constraint fkEmpresaFuncionario foreign key (fk_empresa) references empresa(idEmpresa)
 );
 
 -- TABELA DO SENSOR
@@ -51,20 +59,18 @@ CREATE TABLE sensor(
     pos_y_sensor int,
     pos_z_sensor int,
     numero_rack int,
-    fk_empresa_sensor int,
-    constraint foreign key (fk_empresa_sensor) references empresa(id_empresa)
+	numeroSala int,
+    fkEmpresa int,
+    constraint fkEmpresaSensor foreign key (fkEmpresa) references empresa(idEmpresa)
 );
 
  -- TABELA DE DADOS DO SENSOR --
 
 CREATE TABLE dados(
 	idDados int primary key auto_increment,
-    data_hora dateTime default current_timestamp,
+    dataHora dateTime default current_timestamp,
     temperatura double,
     umidade double,
-    numero_sala int,
     fk_sensor int,
-    constraint foreign key (fk_sensor) references sensor(idSensor)
+    constraint fkSensor foreign key (fk_sensor) references sensor(idSensor)
 );
-
-DROP TABLE dados;
