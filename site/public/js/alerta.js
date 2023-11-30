aea850868ef20e4feea5815ee621f95c52a5e643
Ian Silva Santos <ian.santos@sptech.school>
2023-11-30 18:57:31 -0300

    Começamos a fazer os alertas

--- site/public/js/alerta.js
+++ site/public/js/alerta.js
@@ -1,114 +1,127 @@
 var alertas = [];
 
-function obterdados(idAquario) {
-    fetch(`/medidas/tempo-real/${idAquario}`)
-        .then(resposta => {
-            if (resposta.status == 200) {
-                resposta.json().then(resposta => {
+// function obterdados(idAquario) {
+//     fetch(`/medidas/tempo-real/${idAquario}`)
+//         .then(resposta => {
+//             if (resposta.status == 200) {
+//                 resposta.json().then(resposta => {
 
-                    console.log(`Dados recebidos: ${JSON.stringify(resposta)}`);
+//                     console.log(`Dados recebidos: ${JSON.stringify(resposta)}`);
 
-                    alertar(resposta, idAquario);
-                });
-            } else {
-                console.error(`Nenhum dado encontrado para o id ${idAquario} ou erro na API`);
-            }
-        })
-        .catch(function (error) {
-            console.error(`Erro na obtenção dos dados do aquario p/ gráfico: ${error.message}`);
-        });
+//                     alertar(resposta, idAquario);
+//                 });
+//             } else {
+//                 console.error(`Nenhum dado encontrado para o id ${idAquario} ou erro na API`);
+//             }
+//         })
+//         .catch(function (error) {
+//             console.error(`Erro na obtenção dos dados do aquario p/ gráfico: ${error.message}`);
+//         });
+// }
 
-}
-
-function alertar(resposta, idAquario) {
-    var temp = resposta[0].temperatura;
+function alertar(temperaturaSensor, idRack) {
+    var temp = temperaturaSensor;
 
     var grauDeAviso = '';
 
     var limites = {
-        muito_quente: 23,
-        quente: 22,
-        ideal: 20,
-        frio: 10,
-        muito_frio: 5
+        critico_quente: 40,
+        emer_quente: 34,
+        alert_quente: 30,
+        ideal: 24,
+        alert_frio: 23.8,
+        emer_frio: 19.5,
+        critico_frio: 18
     };
 
-    var classe_temperatura = 'cor-alerta';
+    var classe_temperatura = 'esfera_ideal';
 
-    if (temp >= limites.muito_quente) {
-        classe_temperatura = 'cor-alerta perigo-quente';
-        grauDeAviso = 'perigo quente'
-        grauDeAvisoCor = 'cor-alerta perigo-quente'
-        exibirAlerta(temp, idAquario, grauDeAviso, grauDeAvisoCor)
+    if (temp >= limites.critico_quente) {
+        classe_temperatura = 'esfera_critica';
+        grauDeAviso = 'Temperaturas muito elevadas'
+        grauDeAvisoCor = 'esfera_critica'
+        exibirAlerta(temp, idRack, grauDeAviso, grauDeAvisoCor)
     }
-    else if (temp < limites.muito_quente && temp >= limites.quente) {
-        classe_temperatura = 'cor-alerta alerta-quente';
-        grauDeAviso = 'alerta quente'
-        grauDeAvisoCor = 'cor-alerta alerta-quente'
-        exibirAlerta(temp, idAquario, grauDeAviso, grauDeAvisoCor)
+    else if (temp < limites.critico_quente && temp >= limites.emer_quente) {
+        classe_temperatura = 'esfera_emer';
+        grauDeAviso = 'Temperaturas elevadas'
+        grauDeAvisoCor = 'esfera_emer'
+        exibirAlerta(temp, idRack, grauDeAviso, grauDeAvisoCor)
     }
-    else if (temp < limites.quente && temp > limites.frio) {
-        classe_temperatura = 'cor-alerta ideal';
-        removerAlerta(idAquario);
+    else if(temp < limites.emer_quente && temp >= limites.alert_quente){
+        classe_temperatura = 'esfera_irregular';
+        grauDeAviso = 'Temperaturas estão ficando elevadas'
+        grauDeAvisoCor = 'esfera_irregular'
+        exibirAlerta(temp, idRack, grauDeAviso, grauDeAvisoCor)
     }
-    else if (temp <= limites.frio && temp > limites.muito_frio) {
-        classe_temperatura = 'cor-alerta alerta-frio';
-        grauDeAviso = 'alerta frio'
-        grauDeAvisoCor = 'cor-alerta alerta-frio'
-        exibirAlerta(temp, idAquario, grauDeAviso, grauDeAvisoCor)
+    else if (temp < limites.alert_quente && temp > limites.alert_frio) {
+        classe_temperatura = 'esfera_ideal';
+        removerAlerta(idRack);
+
+    }else if(temp < limites.alert_frio && temp > limites.emer_frio){
+        classe_temperatura = 'esfera_irregular';
+        grauDeAviso = 'Temperaturas estão ficando abaixo do esperado'
+        grauDeAvisoCor = 'esfera_emer'
+        exibirAlerta(temp, idRack, grauDeAviso, grauDeAvisoCor)
     }
-    else if (temp <= limites.muito_frio) {
-        classe_temperatura = 'cor-alerta perigo-frio';
-        grauDeAviso = 'perigo frio'
-        grauDeAvisoCor = 'cor-alerta perigo-frio'
-        exibirAlerta(temp, idAquario, grauDeAviso, grauDeAvisoCor)
+    else if (temp <= emer_frio && temp > limites.critico_frio) {
+        classe_temperatura = 'esfera_emer';
+        grauDeAviso = 'Temperaturas estão ficando baixas'
+        grauDeAvisoCor = 'esfera_irregular'
+        exibirAlerta(temp, idRack, grauDeAviso, grauDeAvisoCor)
+    }
+    else if (temp <= limites.critico_frio) {
+        classe_temperatura = 'esfera_critica';
+        grauDeAviso = 'Temperaturas muito baixas'
+        grauDeAvisoCor = 'esfera_critica'
+        exibirAlerta(temp, idRack, grauDeAviso, grauDeAvisoCor)
     }
 
-    var card;
+    // var card;
 
-    if (document.getElementById(`temp_aquario_${idAquario}`) != null) {
-        document.getElementById(`temp_aquario_${idAquario}`).innerHTML = temp + "°C";
-    }
+    // if (document.getElementById(`temp_aquario_${idAquario}`) != null) {
+    //     document.getElementById(`temp_aquario_${idAquario}`).innerHTML = temp + "°C";
+    // }
 
-    if (document.getElementById(`card_${idAquario}`)) {
-        card = document.getElementById(`card_${idAquario}`)
-        card.className = classe_temperatura;
-    }
+    // if (document.getElementById(`card_${idRack}`)) {
+    //     card = document.getElementById(`card_${idRack}`)
+    //     card.className = classe_temperatura;
+    // }
 }
 
-function exibirAlerta(temp, idAquario, grauDeAviso, grauDeAvisoCor) {
-    var indice = alertas.findIndex(item => item.idAquario == idAquario);
+function exibirAlerta(temp, idRack, grauDeAviso, grauDeAvisoCor) {
+    var indice = alertas.findIndex(item => item.idRack == idRack);
 
     if (indice >= 0) {
-        alertas[indice] = { idAquario, temp, grauDeAviso, grauDeAvisoCor }
+        alertas[indice] = { idRack, temp, grauDeAviso, grauDeAvisoCor }
     } else {
-        alertas.push({ idAquario, temp, grauDeAviso, grauDeAvisoCor });
+        alertas.push({ idRack, temp, grauDeAviso, grauDeAvisoCor });
     }
 
     exibirCards();
 }
 
-function removerAlerta(idAquario) {
-    alertas = alertas.filter(item => item.idAquario != idAquario);
+function removerAlerta(idRack) {
+    alertas = alertas.filter(item => item.idRack != idRack);
     exibirCards();
 }
 
 function exibirCards() {
-    alerta.innerHTML = '';
+    card_alerta.innerHTML = '';
 
     for (var i = 0; i < alertas.length; i++) {
         var mensagem = alertas[i];
-        alerta.innerHTML += transformarEmDiv(mensagem);
+        card_alerta.innerHTML += transformarEmDiv(mensagem);
     }
 }
 
-function transformarEmDiv({ idAquario, temp, grauDeAviso, grauDeAvisoCor }) {
+function transformarEmDiv({ idRack, temp, grauDeAviso, grauDeAvisoCor }) {
 
-    var descricao = JSON.parse(sessionStorage.AQUARIOS).find(item => item.id == idAquario).descricao;
+    var descricao = JSON.parse(sessionStorage.SALAS).find(item => item.idRack == idRack).numeroSala;
     return `
     <div class="mensagem-alarme">
         <div class="informacao">
-            <div class="${grauDeAvisoCor}">&#12644;</div> 
+            <div class="${grauDeAvisoCor}"></div> 
             <h3>${descricao} está em estado de ${grauDeAviso}!</h3>
             <small>Temperatura ${temp}.</small>   
         </div>
@@ -117,9 +130,9 @@ function transformarEmDiv({ idAquario, temp, grauDeAviso, grauDeAvisoCor }) {
     `;
 }
 
-function atualizacaoPeriodica() {
-    JSON.parse(sessionStorage.AQUARIOS).forEach(item => {
-        obterdados(item.id)
-    });
-    setTimeout(atualizacaoPeriodica, 5000);
-}
+// function atualizacaoPeriodica() {
+//     JSON.parse(sessionStorage.SALAS).forEach(item => {
+//         obterdados(item.id)
+//     });
+//     setTimeout(atualizacaoPeriodica, 5000);
+// }
